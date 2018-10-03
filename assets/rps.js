b2e5aa232d163e1777fb138cc4411f24c0f25068
sahenke13 <shenke13@gmail.com>
2018-10-02 20:51:15 -0500

    HW7V1 basic html and depository setup

---
+++ assets/rps.js
@@ -0,0 +1,16 @@
+  
+  
+  
+  
+  
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALk3J0LXvUj3OfH35VKNBt_fIAedLJTO8",
+    authDomain: "inclass-firebase-steven.firebaseapp.com",
+    databaseURL: "https://inclass-firebase-steven.firebaseio.com",
+    projectId: "inclass-firebase-steven",
+    storageBucket: "inclass-firebase-steven.appspot.com",
+    messagingSenderId: "426217518370"
+  };
+  firebase.initializeApp(config);
